7068bb21ecd6c2961d7c9624e5556ac9146fcd63
Slor <22tamikill11@gmail.com>
2022-06-06 12:04:43 +0300

    Solution without taking into account the amount of volumes

--- 12 cube problem/Program.cs
+++ 12 cube problem/Program.cs
@@ -1,82 +1,84 @@
-﻿//длина всех ребер, полная поверхнать, обьем
-class Programm
+﻿class Programm
 {
     static void Main(string[] args)
     {
         int count = 0;
         int lentgh = 20;
+        //Variable for writing the result to a file
         StreamWriter outputFile = new StreamWriter(@"C:\cue\True.txt");
-        Box[] boxes = new Box[lentgh];
+
+        //Output characteristics
+        Cube[] cubes = new Cube[lentgh];
         for (int i = 0; i < lentgh; i++)
         {
-            boxes[i] = new Box(i + 1);
+            cubes[i] = new Cube(i + 1);
         }
 
-
-        foreach (Box box1 in boxes)
+        foreach (Cube cube1 in cubes)
         {
-            Console.Write(box1.edge.ToString() + " - ребро; " +
-                box1.area.ToString() + " - площадь поверхности; " +
-                box1.allEdges.ToString() + " - сумма всех ребер; " +
-                box1.volume.ToString() + " - обьем;");
+            Console.Write(cube1.edge.ToString() + " - edge; " +
+                cube1.area.ToString() + " - area; " +
+                cube1.allEdges.ToString() + " - allEdges; " +
+                cube1.volume.ToString() + " - volume;");
             Console.WriteLine();
         }
-        List<int> fst = new List<int>();
-        List<int> snd = new List<int>();
 
-        Random rand = new Random();
 
-        int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12;
+        List<int> fst = new List<int>(); //Array for entering elements of the first group
+        List<int> snd = new List<int>(); //Array for entering elements of the second group
+
+        Random rand = new Random(); //Variable to create a random number
+
+        int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12; //Variables for entering random numbers
 
         int length13 = 13;
-    start:
-        fst.Clear();
-        //11111
+
+        start:
+        fst.Clear(); //Clearing the first array
+
         for (int a = 1; a < length13; a++)
         {
-            if (fst.ToArray().Sum() < 39)
+            if (fst.ToArray().Sum() < 39) //If the sum of first array is less than 39 create a new number and add to array
             {
                 a1 = rand.Next(1, 13);
                 fst.Add(a1);
             }
-            else if (fst.ToArray().Sum() == 39)
+            else if (fst.ToArray().Sum() == 39) //If the sum of the array is 39 go to the creation of the second group
             {
                 goto startSecondArr;
 
             }
-            else
+            else //If the sum of the array is greater than 39, return to the creation of the first array
             {
                 goto start;
             }
-            //22222
-            for (int b = 1; b < length13 - 1; b++)
+            for (int b = 1; b < length13 - 1; b++) //This and subsequent loops are identical
             {
-                if(fst.ToArray().Sum() < 39)
+                if(fst.ToArray().Sum() < 39) //If the sum of first array is less than 39 create a new number and add to array
                 {
                     while (true)
                     {
                         a2 = rand.Next(1, 13);
-                        if (fst.Contains(a2) == true)
+                        if (fst.Contains(a2) == true) //If the created number is already in the array
                         {
                             continue;
                         }
-                        else
+                        else //If the created number is not in the array, add it
                         {
-                            fst.Add(a2);
+                            fst.Add(a2); 
                             break;
                         }
                     }
                 }
-                else if (fst.ToArray().Sum() == 39)
+                else if (fst.ToArray().Sum() == 39)  //If the sum of the array is 39 go to the creation of the second group
                 {
                     goto startSecondArr;
 
                 }
-                else
+                else //If the sum of the array is greater than 39, return to the creation of the first array
                 {
                     goto start;
                 }
-                //33333
                 for (int c = 1; c < length13 - 2; c++)
                 {
 
@@ -105,7 +107,6 @@ class Programm
                     {
                         goto start;
                     }
-                    //44444
                     for (int d = 1; d < length13 - 3; d++)
                     {
 
@@ -134,8 +135,6 @@ class Programm
                         {
                             goto start;
                         }
-
-                        //55555
                         for (int e = 1; e < length13 - 4; e++)
                         {
                             if (fst.ToArray().Sum() < 39)
@@ -163,7 +162,6 @@ class Programm
                             {
                                 goto start;
                             }
-                            //66666
                             for (int f = 1; f < length13 - 5; f++)
                             {
 
@@ -192,8 +190,6 @@ class Programm
                                 {
                                     goto start;
                                 }
-
-                                //77777
                                 for (int g = 1; g < length13 - 6; g++)
                                 {
                                     if (fst.ToArray().Sum() < 39)
@@ -221,7 +217,6 @@ class Programm
                                     {
                                         goto start;
                                     }
-                                    //88888
                                     for (int h = 1; h < length13 - 7; h++)
                                     {
                                         if (fst.ToArray().Sum() < 39)
@@ -249,7 +244,6 @@ class Programm
                                         {
                                             goto start;
                                         }
-                                        //99999
                                         for (int i = 1; i < length13 - 8; i++)
                                         {
                                             if (fst.ToArray().Sum() < 39)
@@ -277,7 +271,6 @@ class Programm
                                             {
                                                 goto start;
                                             }
-                                            //1010101010
                                             for (int j = 1; j < length13 - 9; j++)
                                             {
                                                 if (fst.ToArray().Sum() < 39)
@@ -305,7 +298,6 @@ class Programm
                                                 {
                                                     goto start;
                                                 }
-                                                //1111111111
                                                 for (int k = 1; k < length13 - 10; k++)
                                                 {
                                                     if (fst.ToArray().Sum() < 39)
@@ -333,7 +325,6 @@ class Programm
                                                     {
                                                         goto start;
                                                     }
-                                                    //1212121212
                                                     for (int l = 1; l < length13 - 11; l++)
                                                     {
                                                         if (fst.ToArray().Sum() < 39)
@@ -374,30 +365,30 @@ class Programm
             }
         }
 
-    startSecondArr:
+        startSecondArr:
 
-        int q = fst.Count;
-        for (int i = 1; i < 13; i++)
+        //The second array consists of numbers from 1 to 12 which were not in the first array
+        for (int i = 1; i < 13; i++) //Creating a second array
         {
-            if(fst.Contains(i) == true)
+            if(fst.Contains(i) == true) //If the number is already in the first array
             {
                 continue;
             }
-            else
+            else //If the number is not in the first array
             {
                 snd.Add(i);
             }
         }
 
-
-        Console.WriteLine("Первая группа:");
+        //Displaying group values
+        Console.WriteLine("First group:");
         foreach (int f in fst)
         {
             Console.Write(f.ToString() + " ");
         }
         Console.WriteLine();
 
-        Console.WriteLine("Вторая группа:");
+        Console.WriteLine("Second group:");
         foreach (int f in snd)
         {
             Console.Write(f.ToString() + " ");
@@ -405,43 +396,69 @@ class Programm
         Console.WriteLine();
 
 
-        List<Box> boxes1 = new List<Box>();
+        
+        //Creating an array of cubes and creating groups
+        List<Cube> cubes1 = new List<Cube>();
         foreach(int f in fst)
         {
-            Box box = new Box(f);
-            boxes1.Add(box);
+            Cube cube = new Cube(f);
+            cubes1.Add(cube);
         }
-        Group group1 = new Group(boxes1);
+        GroupOfCubes group1 = new GroupOfCubes(cubes1);
 
 
-        List<Box> boxes2 = new List<Box>();
+        List<Cube> cubes2 = new List<Cube>();
         foreach (int f in snd)
         {
-            Box box = new Box(f);
-            boxes2.Add(box);
+            Cube cube = new Cube(f);
+            cubes2.Add(cube);
         }
-        Group group2 = new Group(boxes2);
+        GroupOfCubes group2 = new GroupOfCubes(cubes2);
 
-        bool isTrue = EqualsGroups(group1, group2);
-        if (isTrue == false)
+      
+        bool isTrue = EqualsGroups(group1, group2); //Compare group values
+        if (isTrue == false) //If the group values are equal
         {
             Console.WriteLine(isTrue.ToString());
-            Console.WriteLine(count + " - комбинаций проверено");
+            Console.WriteLine(count + " - combinations tested");
             Console.WriteLine();
+
+            //Clearing all variables
             fst.Clear();
             snd.Clear();
-            boxes1.Clear();
-            boxes2.Clear();
+            cubes1.Clear();
+            cubes2.Clear();
             group1 = null;
             group2 = null;
+
             count++;
+
             goto start;
 
         }
-        else
+        else //If the groups are equal
         {
-            Console.WriteLine("БАЛДЕЕЕТЬ");
+            //Console output
+            Console.WriteLine("Groups are equal");
 
+            Console.WriteLine("////////////////////");
+            Console.WriteLine("First group:");
+            foreach (int f in fst)
+            {
+                Console.Write(f.ToString() + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Second group:");
+            foreach (int f in snd)
+            {
+                Console.Write(f.ToString() + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("////////////////////");
+            Console.WriteLine();
+
+            //Write to file
             outputFile.WriteLine("First group:");
             foreach (int f in fst)
             {
@@ -461,7 +478,9 @@ class Programm
 
 
     }
-    public static bool EqualsGroups(Group boxes1, Group boxes2)
+
+    //Group Comparison Method
+    public static bool EqualsGroups(GroupOfCubes cubes1, GroupOfCubes cubes2)
     {
         //Console.WriteLine("1 группа     2 группа");
         //Console.WriteLine(boxes1.sumEdge.ToString() + " " + boxes2.sumEdge.ToString());
@@ -469,10 +488,12 @@ class Programm
         //Console.WriteLine(boxes1.sumAllEges.ToString() + " " + boxes2.sumAllEges.ToString());
         //Console.WriteLine(boxes1.sumVolume.ToString() + " " + boxes2.sumVolume.ToString());
 
-        if (boxes1.sumEdge == boxes2.sumEdge &&
-            boxes1.sumArea == boxes2.sumArea &&
-            boxes1.sumAllEges == boxes2.sumAllEges &&
-            boxes1.sumVolume == boxes2.sumVolume)
+        //Compare all values
+        if (cubes1.sumEdge == cubes2.sumEdge &&
+            cubes1.sumArea == cubes2.sumArea &&
+            cubes1.sumAllEges == cubes2.sumAllEges) //&&
+            //The sum of volumes is not taken into account in the comparison
+            //cubes1.sumVolume == cubes2.sumVolume)
         {
             return true;
         }
@@ -483,39 +504,40 @@ class Programm
     }
 }
 
-class Box
+//Cube class
+class Cube
 {
     public int edge;
     public int area;
     public int allEdges;
     public int volume;
-    public Box(int edge)
+    public Cube(int edge)
     {
         this.edge = edge;
         this.area = edge * edge * 6;
         this.allEdges = edge * 12;
         this.volume = edge * edge * edge;
-
-        //public bool 
     }
 }
-class Group
+
+//Group of cubes class
+class GroupOfCubes
 {
-    public List<Box> boxes;
+    public List<Cube> Cubes;
     public int sumEdge;
     public int sumArea;
     public int sumVolume;
     public int sumAllEges;
 
-    public Group(List<Box> boxes)
+    public GroupOfCubes(List<Cube> Cubes)
     {
-        this.boxes = boxes;
-        foreach (Box box in boxes)
+        this.Cubes = Cubes;
+        foreach (Cube cube in Cubes)
         {
-            this.sumEdge += box.edge;
-            this.sumArea += box.area;
-            this.sumVolume += box.volume;
-            this.sumAllEges += box.allEdges;
+            this.sumEdge += cube.edge;
+            this.sumArea += cube.area;
+            this.sumVolume += cube.volume;
+            this.sumAllEges += cube.allEdges;
         }
     }
 }
--- README.md
+++ README.md
@@ -12,3 +12,9 @@ My problem solution:
 
 I used code that compared 50000000
 groups and did not find a solution. If you find a bug please let me know.
+
+Possible solution:
+
+I removed volume comparisons from the conditions and got an answer. It all comes down to one answer. The first group: 1, 3,
+7, 8, 9, 11. The second group 2, 4, 5, 6, 10, 12. These groups have the same sum of edges, the sum of all edges of cubes,
+the sum of areas.
